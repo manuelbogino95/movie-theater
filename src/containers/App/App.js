e9c52d61f76e66d10b0ebf453313e323abfb0e20
Manuel Bogino <manuelbogino@gmail.com>
2020-08-08 17:09:46 -0300

    Add header

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import styles from "./App.module.css";
 
 function App() {
-  return <p>Movie Theater</p>;
+  return (
+    <div>
+      <Header />
+      <div className={styles.main}></div>
+    </div>
+  );
 }
 
 export default App;
